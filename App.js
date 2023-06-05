159906f3dcabe40e680eaaded2babdb32f4c227f
John Doe <johndoe@example.com>
2023-06-05 16:52:26 +0530

    a

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-slate-500">
+      <Text className="font-semibold text-4xl text-orange-400 my-3">
+        Open up App.js to start working on your app!
+      </Text>
+      <Text className="font-bold text-xl my-3 text-gray-950">Count Value: {count}</Text>
+      <Text
+        className="bg-fuchsia-500 text-2xl px-5 py-2 rounded-xl font-bold "
+        onPress={() => setCount(count + 1)}
+      >
+        Click Me
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
