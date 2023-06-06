24c7d977882a93596c853544c4546765fef25df8
John Doe <johndoe@example.com>
2023-06-06 12:54:35 +0530

    e

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import { Text, View } from "react-native";
 
 export default function App() {
   const [count, setCount] = useState(0);
+
   return (
     <View className="flex-1 justify-center items-center bg-slate-500">
       <Text className="font-semibold text-4xl text-orange-400 my-3">
         Open up App.js to start working on your app!
       </Text>
-      <Text className="font-bold text-xl my-3 text-gray-950">Count Value: {count}</Text>
+      <Text className="font-bold text-xl my-3 text-gray-950">
+        Count Value: {count}
+      </Text>
       <Text
-        className="bg-fuchsia-500 text-2xl px-5 py-2 rounded-xl font-bold "
+        className="bg-fuchsia-500 text-2xl px-5 py-2 rounded-xl font-bold active:bg-fuchsia-400 active:scale-x-90 active:scale-y-95 active:border-2 border-orange-300 border-spacing-2"
         onPress={() => setCount(count + 1)}
       >
         Click Me
